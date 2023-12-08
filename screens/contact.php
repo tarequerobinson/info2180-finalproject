96bd4543712be909971d6630c50bba70fe6ae427
tarequerobinson <tarequerobinson@gmail.com>
2023-12-07 22:14:25 -0500

    added name attribute to form fields

--- screens/contact.php
+++ screens/contact.php
@@ -1,11 +1,15 @@
 <?php 
     require_once("../database/dbsetup.php");
+
+   
+
     $sql = "SELECT * FROM contacts";
     $results = $conn->query($sql); 
 
     if (!$results) {
         die("Error: " . $conn->error);
     }    
+
 ?>
 
 
@@ -23,7 +27,7 @@
 <h1>New Contact</h1>
 
 
-<form id = "newContactForm" action = ".php" method = "POST">
+<form id = "newContactForm" action = "database/contactConnect.php" method = "POST">
 
     <div id = "formColumn1">
 
@@ -32,7 +36,7 @@
                 <label for= "firstname"> First Name<label>
             </div>
             <div>
-                <input id = "firstname" type="text" required placeholder="Jane">
+                <input id = "firstname" type="text" required placeholder="Jane" name="firstname">
             </div>
         </div>
 
@@ -42,7 +46,7 @@
             <div>
             <div>
 
-                <input id = "email" type="text" required placeholder="something@example.com">
+                <input id = "email" type="text" required placeholder="something@example.com" name="email">
             </div>
         </div>
 
@@ -51,7 +55,7 @@
                 <label for= "company">Company<label>
             </div>
             <div>
-                <input id = "company" type="select" required >
+                <input id = "company" type="select" required name="company" >
             </div>
         </div>
     </div>
@@ -64,7 +68,7 @@
                 <label for= "lastname">Lastname<label>
             </div>
             <div>
-                <input id = "lastname" type="text" required>
+                <input id = "lastname" type="text" required name="lastname">
             </div>
         </div>
 
@@ -73,7 +77,7 @@
                 <label for= "telephone"> Telephone<label>
             </div>
             <div>
-                <input id = "telephone" type="text" required placeholder="Doe">
+                <input id = "telephone" type="text" required placeholder="Doe" name="telephone">
             </div>
         </div>
 
@@ -82,7 +86,7 @@
                 <label for= "type"> Type<label>
             </div>
             <div>
-                <input id = "type" type="text" required placeholder="Doe">
+                <input id = "type" type="text" required placeholder="Doe" name="type">
             </div>
         </div>
 
@@ -98,11 +102,5 @@
 </html>
 
 
-<?php
-// content/home.php
-
-
-// echo "This is the content for the New Contact screen.";
-?>
 
 
