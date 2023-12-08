b7c384f6466b6c0d8ab2d104ef1ab39fa043d954
tarequerobinson <tarequerobinson@gmail.com>
2023-12-07 21:47:04 -0500

    added the php file which handles adding a contact to the database after form submission

---
+++ database/contactConnect.php
@@ -0,0 +1,29 @@
+<?php 
+require_once("dbsetup.php");
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+        $firstname = isset($_POST['firstname']) ? $_POST['firstname'] : '';
+        $lastname = isset($_POST['lastname']) ? $_POST['lastname'] : '';
+        $email = isset($_POST['email']) ? $_POST['email'] : '';
+        $type = isset($_POST['type']) ? $_POST['type'] : '';
+        $telephone = isset($_POST['telephone']) ? $_POST['telephone'] : '';
+        $company = isset($_POST['company']) ? $_POST['company'] : '';
+
+
+    
+        $stmt = $conn->prepare("INSERT INTO contacts (firstname, lastname, email, type , telephone , company) VALUES (?, ?, ?, ?, ? , ?)");
+    // Bind parameters to the prepared statement
+    $stmt->bindParam(1, $firstname);
+    $stmt->bindParam(2, $lastname);
+    $stmt->bindParam(3, $email);
+    $stmt->bindParam(4, $type);
+    $stmt->bindParam(5, $telephone);
+    $stmt->bindParam(6, $company);
+
+
+    // Execute the statement
+    $stmt->execute();
+    // $stmt->close();
+    }
+
+    ?>
