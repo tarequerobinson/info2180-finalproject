9f6af4fd86744c6e09fcd5d6964280bff376cc22
tarequerobinson <tarequerobinson@gmail.com>
2023-12-09 13:36:25 -0500

    changes made to update database schema to include time for created_on and display it on contactDetails Page

--- screens/contactDetails.php
+++ screens/contactDetails.php
@@ -15,7 +15,7 @@ if ($stmt) {
     // Check if the result is not empty
     if ($result) {
         // Output the result or use it as needed
-        // print_r($result);
+        print_r($result);
     } else {
         echo "No contact found with the provided ID.";
     }
@@ -56,12 +56,16 @@ if ($notesStmt) {
 
     <div id="contactDetailsHead">
         <h1> <?= $result['firstname'] ?> <?= $result['lastname'] ?> </h1>
+        <p> Created On: <?= $result['created_on'] ?> <?= $result['updated_at'] ?> </p>
+        <p> Updated At: <?= $result['created_on'] ?> <?= $result['updated_at'] ?> </p>
+
+
         <?php
         // Conditionally display buttons based on the user type
         if ($result['type'] === "Support") {
-            echo '<button id = "switch">Switch to Support</button>';
+            echo '<button id = "switch">Switch to Sales</button>';
         } elseif ($result['type'] === "Sales Lead") {
-            echo '<button id = "switch">Switch To Sales Lead</button>';
+            echo '<button id = "switch">Switch To Support</button>';
         }
         ?>
     </div>
