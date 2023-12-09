947135e33332c4b5f18b6a698c5bb1d0b193965e
Odnt <remyalexd2003@gmail.com>
2023-12-08 20:32:03 -0500

    Added Scripts to contactDetails

--- screens/contactDetails.php
+++ screens/contactDetails.php
@@ -42,55 +42,51 @@ if ($notesStmt) {
     echo "Error: " . $conn->errorInfo()[2];
 }
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Details</title>
-    <script src = js/contactDetails.js></script>
-
+    <script src="js/contactDetails.js"></script>
 </head>
 <body>
 
 <div id="contactDetailsHead">
-    <h1> <?= $result['firstname'] ?> <?= $result['lastname'] ?> </h1>
+    <h1><?= $result['firstname'] ?> <?= $result['lastname'] ?></h1>
     <button>Assign TO</button>
     <button>Assign TO</button>
-
 </div>
 
-    <div id="contactDetailsBody">
-        Email: <?= $result['email'] ?>
-        Telephone: <?= $result['telephone'] ?>
-        Company: <?= $result['company'] ?>
-        Assigned to: <?= $result['assigned_to']?> 
-        <!-- ignore ^ -->
-    </div>
+<div id="contactDetailsBody">
+    Email: <?= $result['email'] ?>
+    Telephone: <?= $result['telephone'] ?>
+    Company: <?= $result['company'] ?>
+    <div id="assignedTo"></div>
+</div>
 
-    <div id="NotesBody">
-        
-        <?php foreach ($notesResult as $row): ?>
-        <h3>header for user working on function</h2>
+<div id="NotesBody">
+    <?php foreach ($notesResult as $row): ?>
+        <h3>header for user working on function</h3>
         <p><strong><?= $row['comment'] ?> </strong></p>
-        <p> <?= $row['created_at'] ?> </p>   <!-- this is the date I leave this part to you honestly kye -->
-
-        <?php endforeach;?>
-    </div>
-<form action="notesConnect.php" method="post">
-
-    <h1>Add a note about <?= $result['firstname'] ?> </h1>
-    <textarea id="comment" name="comment" value= "comment"></textarea>
-
-    <br>
-
-    <div id='savectrl'> 
-                <button type="submit" id="save">Save</button>
-    </div>
-</form>
-
+        <p><?= $row['created_at'] ?></p>
+    <?php endforeach; ?>
 </div>
+
+<div id="NotesEntry" m>
+    <h1>Add a note about <?= $result['firstname'] ?></h1>
+    <textarea id="comment" name="comment" placeholder="Enter details here"></textarea>
+    <br>
+    <button type="submit" id="save">Save</button>
     
+</div>
+
 </body>
 </html>
+<script>
+    document.addEventListener('DOMContentLoaded', () => {
+        console.log('poopie')
+        getUserbyID(<?= $id ?>);
+        getNoteUserbyID (<?= $id ?>);
+    });
+</script>
