8111ba5f03f91469aaafedee1505125f8553eda4
tarequerobinson <tarequerobinson@gmail.com>
2023-12-10 16:22:06 -0500

    sanitized all input fields in new user form

--- database/newUserConnect.php
+++ database/newUserConnect.php
@@ -5,11 +5,11 @@ $date = new DateTime();
 ('d/m/Y');
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $firstname = isset($_POST['firstname']) ? $_POST['firstname'] : '';
-    $lastname = isset($_POST['lastname']) ? $_POST['lastname'] : '';
-    $email = isset($_POST['email']) ? $_POST['email'] : '';
-    $role = isset($_POST['role']) ? $_POST['role'] : '';
-    $password = isset($_POST['password']) ? $_POST['password'] : '';
+    $firstname = isset($_POST['firstname']) ? htmlspecialchars($_POST['firstname']) : '';
+    $lastname = isset($_POST['lastname']) ? htmlspecialchars($_POST['lastname']) : '';
+    $email = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
+    $role = isset($_POST['role']) ? htmlspecialchars($_POST['role']) : '';
+    $password = isset($_POST['password']) ? htmlspecialchars($_POST['password']) : '';
     $hrfix = intval($date->format("H")) - 1;
     $createdat = $date->format("Y-m-d") . " " . $date->format($hrfix . ":i:s");
     $password = password_hash ($password, PASSWORD_DEFAULT);
